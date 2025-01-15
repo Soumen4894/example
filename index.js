bf49af138100bd1d7bcce287d6475c30d38b403b
Soumen4894 <soumenmandal0008@gmail.com>
2025-01-15 20:45:32 +0530

    changes2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+const express = require('express')
+const app = express();
+const mongoose = require("mongoose")
+
+app.use(express.json());
+
+const PORT = 8000;
+mongoose
+    .connect("mongodb://127.0.0.1:27017/instagram", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+    app.get("/", (req, res) => {
+        return res.json({
+            success: true,
+            message: 'Your server is up and running....'
+        });
+    });
+    
+    app.listen(PORT, () => {
+        console.log(`App is running at ${PORT}`)
+    })
